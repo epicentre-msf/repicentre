c33449fb18ac76902019f828465103011d831f92
Catherine Eisenhauer <ceisenhauer@users.noreply.github.com>
2025-02-10 19:06:55 +0100

    init

---
+++ sessions_core/03_data-verbs_solutions.R
@@ -0,0 +1,28 @@
+# Solutions to the core session Data Verbs
+# Author :  the Fetch team
+# Creation Date : 10/02/2025
+# Last Update : 10/02/2025
+# Description : Contains solutions to the coding exercices
+
+# PACKAGES -----------------------------------------------------------------------------------------
+library(here)
+library(rio)
+library(tidyverse)
+
+
+
+# IMPORT -------------------------------------------------------------------------------------------
+df_linelist <- import(file = here("data", "clean", "moissala_linelist_clean_EN.rds"))
+
+
+
+# BASIC PIPE ---------------------------------------------------------------------------------------
+# select
+# rename
+# filter
+# basic logic
+# mutate to get dates
+# use of pipe
+
+
+
