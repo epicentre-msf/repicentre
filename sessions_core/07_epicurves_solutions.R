05963441eaa471838b2c3dffc337f4fb382c18f6
Mathilde Mousset <mathilde.mousset@epicentre.msf.org>
2025-02-13 19:50:23 +0100

    Update session

--- sessions_core/07_epicurves_solutions.R
+++ sessions_core/07_epicurves_solutions.R
@@ -1,7 +1,7 @@
 # Solutions to the core session 07_epicurves
 # Author: the Fetch team
 # Creation Date: 03/02/2025
-# Last Update: 04/02/2025
+# Last Update: 13/02/2025
 # Description: Model code associated with the epicurve session
 
 
@@ -12,13 +12,12 @@
 library(here)        # Better paths
 library(rio)         # Import files
 library(dplyr)       # Data manipulation
-library(lubridate)   # Deal with dates
 library(ggplot2)     # Todays workhorse
 
 
 ## Import Data ----------------------------------------------------
 df_linelist <- import(here("data", "clean", 
-                           "simulated_measles_ll.rds"))
+                           "moissala_linelist_clean_EN.rds"))
 
 
 # Check import
@@ -29,13 +28,9 @@ View(df_linelist)
 
 # Prepare data ---------------------------------------------
 
-# Add floor date of weeks
-df_linelist <- df_linelist %>%
-  mutate(week_admission = isoweek(date_admission)) 
-
-# Count numbher of cases per week of admission
+# Count number of cases per date of onset
 df_cases <- df_linelist %>%
-  count(week_admission)
+  count(date_onset)
 
 
 # Plot epicurve --------------------------------------------
@@ -43,30 +38,30 @@ df_cases <- df_linelist %>%
 df_cases %>%
   
   # Initialise the plot
-  ggplot(aes(x = week_admission,
+  ggplot(aes(x = date_onset,
              y = n)) +
   
   # Add the columns, with the optional fill argument to add color
   geom_col(fill = "#2E4573") +
   
   # Add title, axes titles and caption
-  labs(x = "ISO Week",
+  labs(x = "Date of onset",
        y = "Measles Cases",
-       title = "Measles in Moissala",
+       title = "Measles cases in Mandoul region (Chad)",
        caption = "Ficticious data") +
   
   # Modify the general theme to improve appearance
-  theme_bw(base_size = 18) # you may have chosen an other theme!
+  theme_bw(base_size = 16) # you may have chosen an other theme!
 
 
 # If you use the aggregated dataset only once, you could count on the fly:
 df_linelist %>%
-  count(week_admission) %>%
-  ggplot(aes(x = week_admission,
+  count(date_onset) %>%
+  ggplot(aes(x = date_onset,
              y = n)) +
   geom_col(fill = "#2E4573") +
-  labs(x = "ISO Week",
+  labs(x = "Date of onset",
        y = "Measles Cases",
-       title = "Measles in Moissala",
+       title = "Measles cases in Mandoul region (Chad)",
        caption = "Ficticious data") +
-  theme_bw(base_size = 18)
+  theme_bw(base_size = 16)
