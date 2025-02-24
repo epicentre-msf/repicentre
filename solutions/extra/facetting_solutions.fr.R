952d18d2e632b1b18b717c913b201960fe748a7e
Mathilde Mousset <mathilde.mousset@epicentre.msf.org>
2025-02-24 10:59:03 +0100

    Déplace les solutions

--- solutions/extra/facetting_solutions.fr.R
+++ solutions/extra/facetting_solutions.fr.R
@@ -31,68 +31,70 @@ View(df_linelist)
 # Jeu de données aggrégées : compte le nombre de patients par 
 # jour (de début des symptomes)
 df_age <- df_linelist %>% 
-  count(date_onset, age_group)
+  count(date_debut, age_groupe)
 
 
 # Graphiques ------------------------------------------------
 
 # Graphique de base
 df_age %>%
-  ggplot(aes(x = date_onset,
+  ggplot(aes(x = date_debut,
              y = n)) +
   geom_col(fill = "#2E4573") +
-  labs(x = 'Date of onset',
-       y = 'Measles Cases',
-       title = 'Measles in Mandoul region (Chad)',
-       caption = 'Ficticious data') +
+  labs(x = "Date d'apparition des symptomes",,
+       y = "Cas de rougeole",,
+       title = "Cas de rougeole dans la région de Mandoul (Tchad)",
+       caption =  'Données fictives') +
   theme_classic(base_size = 15) +
   
-  facet_wrap(vars(age_group))  # Crée les mini plots / facettes
+  facet_wrap(vars(age_groupe))  # Crée les mini plots / facettes
 
 
 # Modifions des options des facettes
 df_age %>%
-  ggplot(aes(x = date_onset,
+  ggplot(aes(x = date_debut,
              y = n)) +
   geom_col(fill = "#2E4573") +
-  labs(x = 'Date of onset',
-       y = 'Measles Cases',
-       title = 'Measles in Mandoul region (Chad)',
-       caption = 'Ficticious data') +
+  labs(x = "Date d'apparition des symptomes",,
+       y = "Cas de rougeole",,
+       title = "Cas de rougeole dans la région de Mandoul (Tchad)",
+       caption =  'Données fictives') +
   theme_classic(base_size = 15) +
   
-  facet_wrap(vars(age_group),
+  facet_wrap(vars(age_groupe),
              ncol = 2,           # Controle le nombre de colonnes
              scales = "free_y")  # Controle les extrémités des axes
 
-# Exercice challenge
+
+
+# Exercice avancé ------------------------------------------------------
 
 # Jeu de données aggrégé seulement par jour. Utilisé pour les
 # barres grises qui sont les mêmes pour toutes les cases
 df_cases <- df_linelist %>% 
-  count(date_onset)
+  count(date_debut)
 
 
 ggplot() +  # Initialise le graphique
   
   # Trace les barres grises
   geom_col(data = df_cases,    # data frame juste pour cette couche
-           aes(x = date_onset,
+           aes(x = date_debut,
                y = n),
            fill = "grey80") +
   
   # Trace les barres bleues au dessus des barres grises, avec un autre data frame
   geom_col(data = df_age,
-           aes(x = date_onset,
+           aes(x = date_debut,
                y = n),
            fill = "#2E4573") +
   
-  labs(x = 'Date of onset',
-       y = 'Measles Cases',
-       title = 'Measles in Mandoul region (Chad)',
-       caption = 'Ficticious data') +
+  labs(x = "Date d'apparition des symptomes",,
+       y = "Cas de rougeole",,
+       title = "Cas de rougeole dans la région de Mandoul (Tchad)",
+       caption =  'Données fictives') +
   
   theme_classic(base_size = 15) +
   
-  facet_wrap(vars(age_group))
+  facet_wrap(vars(age_groupe))
 
