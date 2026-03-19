5273223e52f82605514053bdb558fff157d20d42
cat eisenhauer <eisenhauer.catherine@gmail.com>
2026-03-19 13:12:30 +0100

    final url

--- docs/site_libs/quarto-html/quarto.js
+++ docs/site_libs/quarto-html/quarto.js
@@ -1,8 +1,5 @@
 import * as tabsets from "./tabsets/tabsets.js";
-<<<<<<< HEAD
-=======
 import * as axe from "./axe/axe-check.js";
->>>>>>> main
 
 const sectionChanged = new CustomEvent("quarto-sectionChanged", {
   detail: {},
@@ -830,10 +827,7 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
 });
 
 tabsets.init();
-<<<<<<< HEAD
-=======
 axe.init();
->>>>>>> main
 
 function throttle(func, wait) {
   let waiting = false;
